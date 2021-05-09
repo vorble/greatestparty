4b95d4ca3ebb6d38a0ac24a5c8877bdac829af95
Keith <vorble@cochleoid.com>
2021-05-09 19:34:46 -0400

    Adding basic potions for each stat.

--- item.ts
+++ item.ts
@@ -7,6 +7,11 @@ interface Item {
 
 class ItemInventory {
   potionStrUp1: Item;
+  potionDexUp1: Item;
+  potionConUp1: Item;
+  potionIntUp1: Item;
+  potionWisUp1: Item;
+  potionChaUp1: Item;
 
   constructor() {
     const defaults = { quantity: 0 };
@@ -15,9 +20,64 @@ class ItemInventory {
       name: 'Potion of STR',
       use: (game: Game) => {
         game.party.status.addStatus(game, {
-          name: 'Potion of STR',
+          name: '+STR',
           tock: 1,
           strmod: 1,
+        });
+      },
+    };
+    this.potionDexUp1 = {
+      ...defaults,
+      name: 'Potion of DEX',
+      use: (game: Game) => {
+        game.party.status.addStatus(game, {
+          name: '+DEX',
+          tock: 1,
+          dexmod: 1,
+        });
+      },
+    };
+    this.potionConUp1 = {
+      ...defaults,
+      name: 'Potion of CON',
+      use: (game: Game) => {
+        game.party.status.addStatus(game, {
+          name: '+CON',
+          tock: 1,
+          conmod: 1,
+        });
+      },
+    };
+    this.potionIntUp1 = {
+      ...defaults,
+      name: 'Potion of INT',
+      use: (game: Game) => {
+        game.party.status.addStatus(game, {
+          name: '+INT',
+          tock: 1,
+          intmod: 1,
+        });
+      },
+    };
+    this.potionWisUp1 = {
+      ...defaults,
+      name: 'Potion of WIS',
+      use: (game: Game) => {
+        game.party.status.addStatus(game, {
+          name: '+WIS',
+          tock: 1,
+          wismod: 1,
+        });
+      },
+    };
+    this.potionChaUp1 = {
+      ...defaults,
+      name: 'Potion of CHA',
+      use: (game: Game) => {
+        game.party.status.addStatus(game, {
+          name: '+CHA',
+          tock: 1,
+          chamod: 1,
         });
       },
     };
