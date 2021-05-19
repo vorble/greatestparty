10252bcdcdfd3adba2631fc9112843ed84beca27
Keith <vorble@cochleoid.com>
2021-05-19 17:45:52 -0400

    Some potential stats for Xo in Spindling Plains. More chance to get potions as loot. Start of logical mechanisms for getting lost in the desert. Added Caravan Scorpion enemy.

--- levels/level02a.ts
+++ levels/level02a.ts
@@ -30,6 +30,8 @@ game.registerLevel({
     town.enemyRatio = 0.06;
 
     const townState = {
+      partyInDesert: false,
+      partyDesertKnowledge: 0,
     };
     town.state = townState;
 
@@ -39,7 +41,7 @@ game.registerLevel({
         const name = rollChoice(ITEM_NAMES_BOOST);
         game.party.items[name].quantity += 1;
         game.log('Your party receives 1 ' + game.party.items[name].name + '.');
-      } else if (r < 0.08) {
+      } else if (r < 0.16) {
         const name = rollChoice(ITEM_NAMES_POTION);
         game.party.items[name].quantity += 1;
         game.log('Your party receives 1 ' + game.party.items[name].name + '.');
@@ -58,6 +60,11 @@ game.registerLevel({
       onTownDepart: (game: Game) => {
       },
       doTickActions: (game: Game) => {
+        if (townState.partyInDesert) {
+          if (townState.partyDesertKnowledge < 10000) {
+            ++townState.partyDesertKnowledge;
+          }
+        }
       },
     };
 
@@ -75,9 +82,7 @@ game.registerLevel({
       {
         name: '', // TODO
         weight: 1,
-        predicate: (game: Game) => {
-          return true;
-        },
+        predicate: (game: Game) => !townState.partyInDesert,
         action: (game: Game) => {
         },
       },
@@ -86,6 +91,7 @@ game.registerLevel({
     town.enemies = [
       // Prarie Hound will start above ground, but can go below to increase
       // defense. This will also reduce its attack.
+      // TODO: Maybe it's more exciting to start burrowed and them pop up.
       {
         weight: 1,
         roll: (game: Game) => {
@@ -164,6 +170,71 @@ game.registerLevel({
           return self;
         },
       },
+      {
+        weight: 1,
+        roll: (game: Game) => {
+          const state = {
+          };
+
+          const self: Enemy = {
+            name: 'Caravan Scorpion',
+            health: 50,
+            str: 5,  int: 8,
+            dex: 6,  wis: 5,
+            con: 14, cha: 4,
+            weapon: {
+              physical: 30,
+              magical: 0,
+              elemental: 0,
+            },
+            armor: {
+              physical: -8,
+              magical: 0,
+              elemental: 0,
+            },
+            state,
+            events: [
+              {
+                name: 'Posture',
+                weight: 3,
+                action: (game: Game) => {
+                  game.log(rollChoice([
+                    'Caravan Scorpion postures for battle.',
+                    'Caravan Scorpion flexes its pincers.',
+                    'Caravan Scorpion undulates its metasoma.',
+                  ]));
+                },
+              },
+              {
+                name: 'Sting',
+                weight: 1,
+                action: (game: Game) => {
+                  const r = (rollDie(20)
+                    + modLinear(game.party.dex, 11)
+                    + modLinear(game.party.con, 14)
+                  );
+                  if (r <= 0) {
+                    game.log('Caravan Scorpion thrusts its stinger at a member of your party and punctures their throat, killing them.');
+                    game.killPartyMembers(1);
+                  } else if (r <= 10) {
+                    game.log('Caravan Scorpion thrusts its stinger at a member of your party and injects them with a burning venom.');
+                    game.party.status.poison.active = true;
+                    setStatusExpiry(game, game.party.status.poison, { term: 1 });
+                  } else {
+                    game.log('Caravan Scorpion thrusts its stinger at your party, but misses.');
+                  }
+                },
+              },
+            ],
+            win: (game: Game) => {
+              game.receiveGold(rollRange(15, 20));
+              loot(game);
+            },
+          };
+
+          return self;
+        },
+      },
     ];
 
     town.bosses = [
@@ -174,20 +245,21 @@ game.registerLevel({
           };
 
           return {
-            name: '', // TODO
-            health: 4000,
-            str:  7, int: 17,
-            dex:  8, wis: 16,
-            con: 11, cha: 12,
+            // Xo - Guardian of Destruction
+            name: 'Xo',
+            health: 5000,
+            str: 25, int: 12,
+            dex:  5, wis: 10,
+            con: 20, cha:  5,
             weapon: {
-              physical: 0,
-              magical: -30,
-              elemental: -60,
+              physical: -60,
+              magical: 30,
+              elemental: 0,
             },
             armor: {
-              physical: 25,
-              magical: -25,
-              elemental: 10,
+              physical: 40,
+              magical: 0,
+              elemental: -25,
             },
             state,
             events: [
